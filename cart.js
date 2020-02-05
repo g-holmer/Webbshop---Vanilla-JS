b49981df1d6f927cc82fdea20a619e84ee916c1c
g-holmer <gabbeholmer@hotmail.com>
2020-02-05 16:42:20 +0100

    working on saving to local storage

--- cart.js
+++ cart.js
@@ -1,14 +1,130 @@
+showCart();
+let prodArray = [];
+
 function createCartList() {
   const buttons = document.querySelectorAll(".btn");
   buttons.forEach(button => {
+    let cartBtnNum = 0;
     button.addEventListener("click", function(evt) {
-      const getName = button.parentElement.previousSibling.firstChild.firstChild.textContent
-      const getImg = button.parentElement.parentElement.previousSibling.firstChild.src
-      const getPrice = button.previousSibling.firstChild.textContent
-      const getAmount = button.previousSibling.previousSibling.firstChild.value
+      const getName = button.parentElement.previousSibling.firstChild.firstChild.textContent;
+      const getImg = button.parentElement.parentElement.previousSibling.firstChild.src;
+      const getPrice = button.previousSibling.firstChild.textContent;
+      const getAmount = button.previousSibling.previousSibling.firstChild.value;
+
+
       
-      console.log(getName, getImg, getPrice, getAmount);
+      const myObject = {
+        name: getName,
+        img: getImg,
+        price: getPrice,
+        amount: getAmount
+      };
+
+      //prodArray.push(myObject);
       
+      prodArray.push(myObject);
+
+      localStorage.setItem("myObject", JSON.stringify(prodArray));
+      
+      let getArray = JSON.parse(localStorage.getItem("myObject"));
+
+
+
+
+
+      /*
+      if (localStorage.getItem("myObject") === null) {
+        localStorage.setItem("prodArray", JSON.stringify(prodArray));
+      }
+      */
+
+      
+
+      //ADD NUMBER TO CART BUTTON
+      cartBtnNum++;
+      const cartButton = document.querySelector(".cart__btn__num-bg");
+      cartButton.style.display = "flex";
+      cartButton.textContent = cartBtnNum;
+
+      //WRITE CART LIST
+      writeCartList(getArray);
     });
+  });
+}
+function writeCartList(getArray) {
+
+  
+  const mainCart = document.querySelector('.main__cart');
+  const productInfo = document.createElement('div');
+  for (let i = 1; i <= getArray.length; i++) {
+    //main product info cart
+    productInfo.className = "products__info " + "product-" + i;
+    mainCart.appendChild(productInfo)
+    const infoImgContainer = document.createElement('div');
+    infoImgContainer.className = 'products__info__img';
+    productInfo.appendChild(infoImgContainer)
+    const infoImg = document.createElement('img');
+    infoImgContainer.appendChild(infoImg)
+    infoImg.className = "product_info_image";
+    //info product name
+    const infoProductName = document.createElement('div');
+    infoProductName.className = 'products__info__productname';
+    productInfo.appendChild(infoProductName)
+    //info sum
+    const infoSum = document.createElement('div');
+    infoSum.className = 'products__info__sum';
+    productInfo.appendChild(infoSum)
+    //info qty
+    const infoQty = document.createElement('div');
+    infoQty.className = 'products__info__qty';
+    productInfo.appendChild(infoQty)
+  }  
+  /*    //total
+    const prodFinish = document.createElement('div');
+    const spanTotalSum = document.createElement('span');
+    const toCartBtn = document.createElement('button');
+    spanTotalSum.textContent = 'Totalsumma:';
+    toCartBtn.textContent = 'To cart';
+    prodFinish.appendChild(spanTotalSum)
+    prodFinish.appendChild(toCartBtn)
+    prodFinish.className = 'products__finish';
+    productInfo.appendChild(prodFinish)
+  */
+  fillCartList(getArray)
+}
+function fillCartList(getArray) {
+  const infoImageDiv = document.querySelectorAll(".product_info_image");
+  const infoNameDiv = document.querySelectorAll(".products__info__productname");
+  const infoPriceDiv = document.querySelectorAll(".products__info__sum");
+
+    //FILL NAMES
+    for (let i = 0; i < getArray.length; i++) {
+      for (let i = 0; i < infoNameDiv.length; i++) {
+        infoNameDiv[i].textContent = getArray[i].name;
+      }
+  
+      //FILL PRICES
+  
+      for (let i = 0; i < infoPriceDiv.length; i++) {
+        infoPriceDiv[i].textContent = getArray[i].price;
+      }
+  
+      //FILL IMAGES
+  
+      for (let i = 0; i < infoImageDiv.length; i++) {
+        infoImageDiv[i].src = getArray[i].img;
+      }
+    }
+
+}
+function showCart() {
+  const cartBtn = document.querySelector(".cart__btn");
+  const cartWindow = document.querySelector(".main__cart");
+  cartBtn.addEventListener("click", function() {
+    if (cartWindow.style.display === "none") {
+      cartWindow.style.display = "flex";
+    } else {
+      cartWindow.style.display = "none";
+    }
   });
 }
--- main.js
+++ main.js
@@ -1,105 +1,119 @@
+//FETCH
 
-    //FETCH
-    fetch("products.json")
-    .then(response => response.json())
-    .then(data => showProducts(data))
-    .catch(err => console.log(err))
+fetch("products.json")
+  .then(response => response.json())
+  .then(data => showProducts(data))
+  .catch(err => console.log(err));
+
+function showProducts(prodItems) {
+  createHtmlStructure(prodItems);
+  fillHtmlStructure(prodItems);
+  createCartList();
+}
+
+function createHtmlStructure(prodItems) {
+  for (let i = 1; i <= prodItems.length; i++) {
+    const productsWrapper = document.querySelector(".products__wrapper");
+    const mainProduct = document.createElement("div");
+    mainProduct.className = "products " + "product-" + i;
+    productsWrapper.appendChild(mainProduct);
+
+    //IMG
+    const imageWrapper = document.createElement("div");
+    imageWrapper.className = "image__div";
+    mainProduct.appendChild(imageWrapper);
+
+    const imgElement = document.createElement("img");
+    imageWrapper.appendChild(imgElement);
+    imgElement.src = "";
+
+    imgElement.className = "product__image";
+
+    //product info wrapper
+    const prodInfoWrapper = document.createElement("div");
+    mainProduct.appendChild(prodInfoWrapper);
+    prodInfoWrapper.className = "prod__info__wrapper";
+    //product info top
+    const prodInfoWrapperTop = document.createElement("div");
+    prodInfoWrapper.appendChild(prodInfoWrapperTop);
+    prodInfoWrapperTop.className = "prod__info__wrapper__top";
+    //product info bottom
+    const prodInfoWrapperBottom = document.createElement("div");
+    prodInfoWrapper.appendChild(prodInfoWrapperBottom);
+    prodInfoWrapperBottom.className = "prod__info__wrapper__bottom";
+    //Product Name
+    const prodNameWrapper = document.createElement("div");
+    const prodNameFont = document.createElement("h4");
+    prodNameWrapper.className = "product__name";
+    prodNameFont.className = "product__name__font";
+    prodInfoWrapperTop.appendChild(prodNameWrapper);
+    prodNameWrapper.appendChild(prodNameFont);
 
 
-    function showProducts(prodItems) {
+    //input btns
+    const inputBtnWrapper = document.createElement("div");
+    inputBtnWrapper.className = "input__buttons-wrapper";
+    prodInfoWrapperBottom.appendChild(inputBtnWrapper);
 
-      createHtmlStructure(prodItems);
-      fillHtmlStructure(prodItems);
-         createCartList();
+    const selectTag = document.createElement("select");
+    inputBtnWrapper.appendChild(selectTag);
+
+    const option1 = document.createElement("option");
+    const option2 = document.createElement("option");
+    const option3 = document.createElement("option");
+    const option4 = document.createElement("option");
+    const option5 = document.createElement("option");
+    selectTag.appendChild(option1);
+    selectTag.appendChild(option2);
+    selectTag.appendChild(option3);
+    selectTag.appendChild(option4);
+    selectTag.appendChild(option5);
+    option1.value = 1;
+    option1.textContent = 1;
+    option2.value = 2;
+    option2.textContent = 2;
+    option3.value = 3;
+    option3.textContent = 3;
+    option4.value = 4;
+    option4.textContent = 4;
+    option5.value = 5;
+    option5.textContent = 5;
+
+    //Product price
+    const prodPriceWrapper = document.createElement("span");
+    prodPriceWrapper.className = "product__price";
+    prodInfoWrapperBottom.appendChild(prodPriceWrapper);
+
+    //button
+    const btn = document.createElement("button");
+    btn.className = "btn";
+    btn.textContent = "Add to cart";
+    prodInfoWrapperBottom.appendChild(btn);
+  }
+}
+
+function fillHtmlStructure(prodItems) {
+  //  FILL CONTENT FROM API
+  const imageDiv = document.querySelectorAll(".product__image");
+  const nameDiv = document.querySelectorAll(".product__name__font");
+  const priceDiv = document.querySelectorAll(".product__price");
+
+  //FILL NAMES
+  for (let i = 0; i < prodItems.length; i++) {
+    for (let i = 0; i < nameDiv.length; i++) {
+      nameDiv[i].textContent = prodItems[i].name;
     }
-
-    function createHtmlStructure(prodItems) {
-      for (let i = 1; i <= prodItems.length; i++) {
-        const productsWrapper = document.querySelector(".products__wrapper")
-        const mainProduct = document.createElement("div")
-        mainProduct.className = "products " + "product-" + i
-        productsWrapper.appendChild(mainProduct)
-  
-        //IMG
-        const imageWrapper = document.createElement("div")
-        imageWrapper.className = "image__div"
-        mainProduct.appendChild(imageWrapper)
-  
-        const imgElement = document.createElement("img")
-        imageWrapper.appendChild(imgElement)
-        imgElement.src = ""
-  
-        imgElement.className = "product__image"
-  
-        //product info wrapper
-        const prodInfoWrapper = document.createElement("div")
-        mainProduct.appendChild(prodInfoWrapper)
-        prodInfoWrapper.className = "prod__info__wrapper"
-        //product info top
-        const prodInfoWrapperTop = document.createElement("div")
-        prodInfoWrapper.appendChild(prodInfoWrapperTop)
-        prodInfoWrapperTop.className = "prod__info__wrapper__top"
-        //product info bottom
-        const prodInfoWrapperBottom = document.createElement("div")
-        prodInfoWrapper.appendChild(prodInfoWrapperBottom)
-        prodInfoWrapperBottom.className = "prod__info__wrapper__bottom"
-        //Product Name
-        const prodNameWrapper = document.createElement("div")
-        const prodNameFont = document.createElement("h4")
-        prodNameWrapper.className = "product__name"
-        prodNameFont.className = "product__name__font"
-        prodInfoWrapperTop.appendChild(prodNameWrapper)
-        prodNameWrapper.appendChild(prodNameFont)
-  
-        //input btns
-        const inputBtnWrapper = document.createElement("div")
-        inputBtnWrapper.className = "input__buttons-wrapper"
-        prodInfoWrapperBottom.appendChild(inputBtnWrapper)
-  
-        const selectTag = document.createElement("select")
-        inputBtnWrapper.appendChild(selectTag)
-  
-        const option = document.createElement("option")
-        selectTag.appendChild(option)
-        option.value = "1"
-  
-        //Product price
-        const prodPriceWrapper = document.createElement("span")
-        prodPriceWrapper.className = "product__price"
-        prodInfoWrapperBottom.appendChild(prodPriceWrapper)
-  
-        //button
-        const btn = document.createElement("button")
-        btn.className = "btn"
-        btn.textContent = "Add to cart"
-        prodInfoWrapperBottom.appendChild(btn)
-      }
-    }
-
-    function fillHtmlStructure(prodItems) {
-    //  FILL CONTENT FROM API
-    const imageDiv = document.querySelectorAll(".product__image")
-    const nameDiv = document.querySelectorAll(".product__name__font")
-    const priceDiv = document.querySelectorAll(".product__price")
-
-    //FILL NAMES
-    for (let i = 0; i < prodItems.length; i++) {
-      for (let i = 0; i < nameDiv.length; i++) {
-        nameDiv[i].textContent = prodItems[i].name
-      }
-    
 
     //FILL PRICES
 
-      for (let i = 0; i < priceDiv.length; i++) {
-        priceDiv[i].textContent = prodItems[i].price
-      }
+    for (let i = 0; i < priceDiv.length; i++) {
+      priceDiv[i].textContent = prodItems[i].price;
+    }
 
     //FILL IMAGES
 
-      for (let i = 0; i < imageDiv.length; i++) {
-        imageDiv[i].src = prodItems[i].image
-      }
+    for (let i = 0; i < imageDiv.length; i++) {
+      imageDiv[i].src = prodItems[i].image;
     }
-
   }
+}
