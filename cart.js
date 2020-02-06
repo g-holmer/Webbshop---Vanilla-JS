f25fa7658dc4751c0d9d80a5b8d2502bcfce1717
g-holmer <gabbeholmer@hotmail.com>
2020-02-06 15:06:55 +0100

    lägger till i varukorg och stannar kvar men går ej att lägga till efter refresh

--- cart.js
+++ cart.js
@@ -1,18 +1,30 @@
 showCart();
-writeCartList();
+refreshCartList();
+updateCartBtn();
 
+function updateCartBtn() {
+  let getArray = JSON.parse(localStorage.getItem("myObject"));
 
+  if(getArray.length !== 0)
+  {
+
+    const cartButton = document.querySelector(".cart__btn__num-bg");
+    cartButton.style.display = "flex";
+    cartButton.textContent = getArray.length;
+  }
+}
 function createCartList() {
+  
   const buttons = document.querySelectorAll(".btn");
   let prodArray = [];
   buttons.forEach(button => {
-    let cartBtnNum = 0;
 
     button.addEventListener("click", function(evt) {
+      
       const getName = button.parentElement.previousSibling.firstChild.firstChild.textContent;
       const getImg = button.parentElement.parentElement.previousSibling.firstChild.src;
       const getPrice = button.previousSibling.firstChild.textContent;
-      const getAmount = button.previousSibling.previousSibling.firstChild.value;
+      const getQty = button.previousSibling.previousSibling.firstChild.value;
 
 
       
@@ -20,26 +32,27 @@ function createCartList() {
         name: getName,
         img: getImg,
         price: getPrice,
-        amount: getAmount
+        qty: getQty
       };
-    
-      prodArray.push(myObject);
 
-      localStorage.setItem("myObject", JSON.stringify(prodArray));
+        prodArray.push(myObject);
 
+        localStorage.setItem("myObject", JSON.stringify(prodArray));
+
+        let getArray = JSON.parse(localStorage.getItem("myObject"));
 
       //ADD NUMBER TO CART BUTTON
-      cartBtnNum++;
+
       const cartButton = document.querySelector(".cart__btn__num-bg");
       cartButton.style.display = "flex";
-      cartButton.textContent = cartBtnNum;
-
+      cartButton.textContent = getArray.length;
+      
       //WRITE CART LIST
       writeCartList();
     });
   });
 }
-function writeCartList() {
+function refreshCartList() {
 
   let getArray = JSON.parse(localStorage.getItem("myObject"));
 
@@ -49,6 +62,8 @@ function writeCartList() {
     //main product info cart
     productInfo.className = "products__info " + "product-" + i;
     mainCart.appendChild(productInfo)
+  
+
     const infoImgContainer = document.createElement('div');
     infoImgContainer.className = 'products__info__img';
     productInfo.appendChild(infoImgContainer)
@@ -67,7 +82,77 @@ function writeCartList() {
     const infoQty = document.createElement('div');
     infoQty.className = 'products__info__qty';
     productInfo.appendChild(infoQty)
-  }  
+  }
+
+  /*    //total
+    const prodFinish = document.createElement('div');
+    const spanTotalSum = document.createElement('span');
+    const toCartBtn = document.createElement('button');
+    spanTotalSum.textContent = 'Totalsumma:';
+    toCartBtn.textContent = 'To cart';
+    prodFinish.appendChild(spanTotalSum)
+    prodFinish.appendChild(toCartBtn)
+    prodFinish.className = 'products__finish';
+    productInfo.appendChild(prodFinish)
+  */
+ refreshFillCartList(getArray)
+}
+function refreshFillCartList(getArray) {
+  
+  const infoImageDiv = document.querySelectorAll(".product_info_image");
+  const infoNameDiv = document.querySelectorAll(".products__info__productname");
+  const infoPriceDiv = document.querySelectorAll(".products__info__sum");
+  const infoQty = document.querySelectorAll(".products__info__qty");
+
+
+  //HÄR SKA DE HÄNDA NÅGOT
+  
+    //FILL NAMES
+
+    for (let i = 0; i < getArray.length; i++) {
+    
+
+      infoNameDiv[i].textContent = getArray[i].name;
+      infoImageDiv[i].src = getArray[i].img;
+      infoPriceDiv[i].textContent = getArray[i].price;
+      infoQty[i].textContent = getArray[i].qty;
+      
+    }
+
+
+
+}
+function writeCartList() {
+  
+  let getArray = JSON.parse(localStorage.getItem("myObject"));
+  const mainCart = document.querySelector('.main__cart');
+  const productInfo = document.createElement('div');
+  
+  for (let i = 1; i <= getArray.length; i++) {
+    //main product info cart
+    productInfo.className = "products__info " + "product-" + getArray.length;
+    mainCart.appendChild(productInfo)
+  }
+
+    const infoImgContainer = document.createElement('div');
+    infoImgContainer.className = 'products__info__img';
+    productInfo.appendChild(infoImgContainer)
+    const infoImg = document.createElement('img');
+    infoImgContainer.appendChild(infoImg)
+    infoImg.className = "product_info_image";
+    //info product name
+    const infoProductName = document.createElement('div');
+    infoProductName.className = 'products__info__productname';
+    productInfo.appendChild(infoProductName)
+    //info sum
+    const infoSum = document.createElement('div');
+    infoSum.className = 'products__info__sum';
+    productInfo.appendChild(infoSum)
+    //info qty
+    const infoQty = document.createElement('div');
+    infoQty.className = 'products__info__qty';
+    productInfo.appendChild(infoQty)
+  
   /*    //total
     const prodFinish = document.createElement('div');
     const spanTotalSum = document.createElement('span');
@@ -83,9 +168,10 @@ function writeCartList() {
 }
 function fillCartList(getArray) {
   
-  const infoImageDiv = document.querySelectorAll(".product_info_image");
   const infoNameDiv = document.querySelectorAll(".products__info__productname");
+  const infoImageDiv = document.querySelectorAll(".product_info_image");
   const infoPriceDiv = document.querySelectorAll(".products__info__sum");
+  const infoQty = document.querySelectorAll(".products__info__qty");
 
 
   //HÄR SKA DE HÄNDA NÅGOT
@@ -93,7 +179,12 @@ function fillCartList(getArray) {
     //FILL NAMES
 
     for (let index = 0; index < getArray.length; index++) {
-      console.log(getArray[index])
+      const lastItem = getArray.length-1;
+
+      infoNameDiv[lastItem].textContent = getArray[lastItem].name;
+      infoImageDiv[lastItem].src = getArray[lastItem].img;
+      infoPriceDiv[lastItem].textContent = getArray[lastItem].price;
+      infoQty[lastItem].textContent = getArray[lastItem].qty;
       
     }
 
